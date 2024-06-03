8e1da8c9aa6d478d739e16689ad624b26976071f
Carolinealt <73542400+Carolinealt@users.noreply.github.com>
2024-06-03 20:35:01 +0300

    validation

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,6 +1,17 @@
 import { Field, Form, Formik } from "formik";
 import css from "./ContactForm.module.css";
+import * as Yup from "yup";
 const ContactForm = ({ onSubmit }) => {
+  let schema = Yup.object().shape({
+    name: Yup.string()
+      .min(2, "too short")
+      .max(50, "too long")
+      .required("required"),
+    number: Yup.string()
+      .min(2, "too short")
+      .max(50, "too long")
+      .required("required"),
+  });
   return (
     <>
       <Formik
@@ -9,15 +20,18 @@ const ContactForm = ({ onSubmit }) => {
           console.log("actions", actions.resetForm());
           onSubmit(values);
         }}
+        validationSchema={schema}
       >
         <Form className={css.form}>
           <label htmlFor="name">
-            <Field type="text" name="name" className={css.field}/>
+            <Field type="text" name="name" className={css.field} />
           </label>
           <label htmlFor="number">
-            <Field type="text" name="number" className={css.field}/>
+            <Field type="text" name="number" className={css.field} />
           </label>
-          <button type="submit" className={css.addBtn}>add</button>
+          <button type="submit" className={css.addBtn}>
+            add
+          </button>
         </Form>
       </Formik>
     </>
